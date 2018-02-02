25935df06a806545aad50a24b59fb1a1896ac423
Jaroslav Šmolík <grissius@gmail.com>
2018-02-02 14:05:25 +0100

    Use vector diagrams

--- bin/diagrams.sh
+++ bin/diagrams.sh
@@ -5,5 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create PNGs from plantuml
 for f in $(find $DIR/../src/assets/diagram -name '*.plantuml'); do
-  plantuml $f
+  plantuml -tsvg $f
+  fname="${f%.*}"
+  inkscape -z --file=$fname.svg --export-pdf=$fname.pdf
+  rm $fname.svg
 done
